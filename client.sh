7d39df27d54ccd5d2a4124cbe4f470897fb75acb
P4tches <P4tches@users.noreply.github.com>
2016-12-17 16:40:11 -0800

    Client Generate ID

--- client.sh
+++ client.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+if [ ! -f set.txt ]; then
+	env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c 10 > set.txt
+fi
 echo $(cat set.txt) | nc 127.0.0.1 8086
 nc 127.0.0.1 8080 > /tmp/execute.sh
 chmod +x /tmp/execute.sh
