1f84944991b15cb8bbcaf531735f93a9a8533786
P4tches <P4tches@users.noreply.github.com>
2016-12-17 16:20:51 -0800

    Generate Configs

--- server.sh
+++ server.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 while :
 do
-nc -l 8086 > id.txt
-tail execute$(cat id.txt).txt | nc -l 8080
 > execute$(cat id.txt).txt
 > id.txt
+nc -l 8086 > id.txt
+tail execute$(cat id.txt).txt | nc -l 8080
+#> execute$(cat id.txt).txt
+#> id.txt
 done
